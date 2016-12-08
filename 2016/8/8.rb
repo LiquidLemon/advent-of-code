0b3d0b8a342d26dea819b2e85c7f658071b707b2
LiquidLemon <liquidlemon@scryptmail.com>
2016-12-08 16:38:14 +0100

    Improve visualization of last solution

--- 2016/8/8.rb
+++ 2016/8/8.rb
@@ -7,6 +7,7 @@ class Display < Matrix
         self[i, j] = true
       end
     end
+    self.print(true)
   end
 
   def rotate_row(row, shift)
@@ -16,6 +17,7 @@ class Display < Matrix
         self[row, x] = self[row, x-1]
       end
       self[row, 0] = last
+      self.print(true)
     end
   end
 
@@ -26,6 +28,7 @@ class Display < Matrix
         self[x, column] = self[x-1, column]
       end
       self[0, column] = last
+      self.print(true)
     end
   end
 
@@ -33,18 +36,21 @@ class Display < Matrix
     self.each.count { |x| x }
   end
 
-  def print
+  def print(clear = false)
     self.each_with_index do |v, row, col|
       $stdout.print (v ? "\u2588" : ' ')
       $stdout.print "\n" if col == 49
     end
+    $stdout.print "\e[6A" if clear
+    sleep 0.07
     nil
   end
 end
 
 INPUT = ARGF.readlines
 display = Display.build(6, 50) { false }
-#require 'pry'; binding.pry
+Signal.trap('SIGINT') { print "\e[?25h"; exit 0 } # show cursor
+print "\e[?25l" # hide cursor
 INPUT.each do |line|
   if match = line.match(/rect (?<width>\d+)x(?<height>\d+)/)
     display.rect(match[:width].to_i, match[:height].to_i)
@@ -55,10 +61,7 @@ INPUT.each do |line|
       display.rotate_row(match[:row].to_i, match[:shift].to_i)
     end
   end
-  display.print
-  print "\e[K" + line
-  print "\e[7A"
-  sleep 0.3
 end
 display.print
 puts display.empty_count
+print "\e[?25h" # show cursor
